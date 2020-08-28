53b037bb0c2144681c941d190c9860f3c6e042b2
Maximo van der Raadt <37664376+InlandTaipan@users.noreply.github.com>
2020-08-28 15:57:19 -0600

    Created DAQ_pressure_to_density
    Created and commented DAQ_pressure_to_density.py. Also fixed typos in constants.py and converted vapour_pressure_calculation.py from static class to dynamic class.

--- vapour_pressure_calculations.py
+++ vapour_pressure_calculations.py
@@ -7,7 +7,7 @@ from math import exp
 
 class VapourPressureCalculations:
     '''
-    Establish the vapour pressure of NOS between -90 and 36 degrees celsius
+    Establish the vapour pressure of NOS between -90 and 36 degrees Celsius
     '''
     def eqn4_1(curr_t_reduced, curr_one_minus_t_reduced):
         '''
@@ -28,15 +28,16 @@ class VapourPressureCalculations:
                  EquationConstants.eqn4_1[3]*curr_one_minus_t_reduced**(2.5) +\
                  EquationConstants.eqn4_1[4]*curr_one_minus_t_reduced**5))
         return result #Return pressure
-    #Temperature values ranging from -90 to 36 degrees celsius
-    t_deg_c = range(-90,37)
-    #Converting t_deg_c into Kelvin
-    t_kelvin = [x + 273.15 for x in tDegC]
-    #Reduced temperature for each temperature step
-    t_reduced = [x / NitrousOxideProperties.critical_temp for x in t_kelvin]
-    #One minus the reduced temperature for each temperature step
-    one_minus_t_reduced = [1 - x for x in t_reduced]
-    #Pressure at each temperature step found using Equation 4.1
-    pressure_kpa = [eqn4_1(x,y) for x,y in zip(t_reduced,one_minus_t_reduced)]
-    #Pressure at each temperature step converted to psi
-    pressure_psi = [pascals_to_psi(x * 1000) for x in pressure_kpa]
+    def __init__(self):
+        #Temperature values ranging from -90 to 36 degrees celsius
+        self.t_deg_c = range(-90,37)
+        #Converting t_deg_c into Kelvin
+        self.t_kelvin = [x + 273.15 for x in tDegC]
+        #Reduced temperature for each temperature step
+        self.t_reduced = [x / NitrousOxideProperties.critical_temp for x in t_kelvin]
+        #One minus the reduced temperature for each temperature step
+        self.one_minus_t_reduced = [1 - x for x in t_reduced]
+        #Pressure at each temperature step found using Equation 4.1
+        self.pressure_kpa = [eqn4_1(x,y) for x,y in zip(t_reduced,one_minus_t_reduced)]
+        #Pressure at each temperature step converted to psi
+        self.pressure_psi = [pascals_to_psi(x * 1000) for x in pressure_kpa]
